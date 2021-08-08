2be22cba1188244a0d072ca974be93f0ecfd0ceb
Enron <286526681@qq.com>
2021-08-08 19:55:23 +0800

    A Little Update
    一点点更新，但是还是没法达到预期效果。

--- src/main/java/iam/xiaoan/Listener/Recall.java
+++ src/main/java/iam/xiaoan/Listener/Recall.java
@@ -1,26 +1,20 @@
 package iam.xiaoan.Listener;
 
-import iam.xiaoan.AntiReCall;
-import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.contact.Group;
+import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import net.mamoe.mirai.event.Listener;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.event.events.MessageRecallEvent;
-import net.mamoe.mirai.message.data.At;
-import net.mamoe.mirai.message.data.MessageChain;
-import net.mamoe.mirai.message.data.MessageUtils;
-import net.mamoe.mirai.message.data.PlainText;
+import net.mamoe.mirai.message.data.*;
 
-import javax.swing.*;
 
 public class Recall {
+    public static boolean onRecall;
+    @EventHandler
     public static void lRecall() {
-            GlobalEventChannel.INSTANCE.subscribeAlways(MessageRecallEvent.GroupRecall.class, messageRecallEvent -> {
-            Group group = messageRecallEvent.getGroup();
-            At at = new At(messageRecallEvent.getAuthorId());
-            AntiReCall.getBot().getGroup(group.getId()).sendMessage(MessageUtils.newChain(at,new PlainText(" 哈哈！你撤回了一条消息，我看到了！")));
-
-
-        });
+            GlobalEventChannel.INSTANCE.subscribeAlways(GroupMessageEvent.class, event1 -> {
+                GlobalEventChannel.INSTANCE.subscribeAlways(MessageRecallEvent.GroupRecall.class, hd ->{
+                    hd.getGroup().sendMessage(MessageUtils.newChain(new At(hd.getAuthorId()),new PlainText(" 哈哈哈，你撤回了一条消息？被我看到啦，是不是："), event1.getMessage()));
+                });
+            });
     }
 }
